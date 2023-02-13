ac92b884680c1283ff36271b9d514944b07c7523
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2023-02-13 09:06:02 +0800

    Fix formatting issues

--- CreatePianoReaperProj/CreateProject.ps1
+++ CreatePianoReaperProj/CreateProject.ps1
@@ -1,29 +1,29 @@
 [CmdletBinding()]
 param (
-	[Parameter(Mandatory = $true)][string]$Search,
-	[Parameter(Mandatory = $false)][bool]$Open
+    [Parameter(Mandatory = $true)][string]$Search,
+    [Parameter(Mandatory = $false)][bool]$Open
 )
 
 if (!([Environment]::OSVersion.Platform -eq 'Win32NT')) {
-	Write-Error 'You must be running on Windows to do this!'
-	Exit 1
+    Write-Error 'You must be running on Windows to do this!'
+    Exit 1
 }
 
 function Test-CommandExistence {
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-		[string]$Command
-	)
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string]$Command
+    )
 
-	# Try to retrieve the command
-	try {
-		Get-Command $Command -ErrorAction Stop
-		return $true
-	}
- catch {
-		return $false
-	}
+    # Try to retrieve the command
+    try {
+        Get-Command $Command -ErrorAction Stop
+        return $true
+    }
+    catch {
+        return $false
+    }
 }
 
 $stopwatch = New-Object Diagnostics.Stopwatch
@@ -33,27 +33,27 @@ $stopwatch.Start()
 
 # Install Chocolatey and the packages
 if (!(Test-CommandExistence 'choco')) {
-	Write-Host 'Installing Chocolatey...'
-	Set-ExecutionPolicy Bypass -Scope Process -Force
-	Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Write-Host 'Installing Chocolatey...'
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-	if (!(Test-CommandExistence 'python')) {
-		Write-Host 'Installing Python...'
-		choco upgrade -y python
-		refreshenv
-	}
+    if (!(Test-CommandExistence 'python')) {
+        Write-Host 'Installing Python...'
+        choco upgrade -y python
+        refreshenv
+    }
 
-	if (!(Test-CommandExistence 'ffmpeg')) {
-		Write-Host 'Installing ffmpeg...'
-		choco upgrade -y ffmpeg
-		refreshenv
-	}
+    if (!(Test-CommandExistence 'ffmpeg')) {
+        Write-Host 'Installing ffmpeg...'
+        choco upgrade -y ffmpeg
+        refreshenv
+    }
 
-	if (!(Test-CommandExistence 'spotdl')) {
-		Write-Host 'Installing spotdl...'
-		python -m pip install spotdl
-		refreshenv
-	}
+    if (!(Test-CommandExistence 'spotdl')) {
+        Write-Host 'Installing spotdl...'
+        python -m pip install spotdl
+        refreshenv
+    }
 }
 
 Start-Process -FilePath 'python' -ArgumentList "-m spotdl `"$Search`"" -NoNewWindow -Wait
@@ -76,26 +76,26 @@ $rpp = $rpp.Replace('E 230400', "E $durationInTicks")
 $rpp = $rpp.Replace('FILE ""', "FILE `"$songFileName.mp3`"")
 $rppFile = [IO.Path]::Combine($songDir, "$songFileName.rpp")
 
-New-Item  -Path		   $songDir	 -ItemType	  Directory -Force
-Move-Item -LiteralPath $songFile -Destination $songDir	-Force
-New-Item  -Path		   $rppFile	 -ItemType	  File		-Force -Value $rpp
+New-Item  -Path        $songDir  -ItemType    Directory -Force
+Move-Item -LiteralPath $songFile -Destination $songDir  -Force
+New-Item  -Path        $rppFile  -ItemType    File      -Force -Value $rpp
 
 if ($Open) {
-	$reaperPath = [IO.Path]::Combine('HKLM:', 'SOFTWARE', 'Microsoft', 'Windows', 'CurrentVersion', 'Uninstall', 'REAPER')
-	if (!(Test-Path $reaperPath)) {
-		Write-Error 'REAPER is not installed.'
-		$stopwatch.Stop()
-		Exit 1
-	}
+    $reaperPath = [IO.Path]::Combine('HKLM:', 'SOFTWARE', 'Microsoft', 'Windows', 'CurrentVersion', 'Uninstall', 'REAPER')
+    if (!(Test-Path $reaperPath)) {
+        Write-Error 'REAPER is not installed.'
+        $stopwatch.Stop()
+        Exit 1
+    }
 
-	$reaperAppPath = Resolve-Path (Get-ItemProperty $reaperPath).InstallLocation
-	if ([string]::IsNullOrEmpty($reaperAppPath)) {
-		Write-Error 'Unable to retrieve REAPER app path.'
-		$stopwatch.Stop()
-		Exit 1
-	}
+    $reaperAppPath = Resolve-Path (Get-ItemProperty $reaperPath).InstallLocation
+    if ([string]::IsNullOrEmpty($reaperAppPath)) {
+        Write-Error 'Unable to retrieve REAPER app path.'
+        $stopwatch.Stop()
+        Exit 1
+    }
 
-	& ([IO.Path]::Combine($reaperAppPath, 'reaper.exe')) $rppFile
+    & ([IO.Path]::Combine($reaperAppPath, 'reaper.exe')) $rppFile
 }
 
 $stopwatch.Stop()
